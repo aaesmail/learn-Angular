0dbe8e4c1f06b11356bf6d13d96550fac6c93f50
Ali Adel <42678489+lFirebolt@users.noreply.github.com>
2021-05-02 01:42:59 +0200

    added pipes

---
+++ pipes/src/app/filter.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "filter",
+  pure: false,
+})
+export class FilterPipe implements PipeTransform {
+  transform(value: any, filterString: string, propName: string): any {
+    if (value.length === 0 || filterString === "") return value;
+
+    const resultArray = [];
+
+    value.forEach((item) =>
+      item[propName].includes(filterString) ? resultArray.push(item) : null
+    );
+
+    return resultArray;
+  }
+}
